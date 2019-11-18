a073b5573f702f81a0a21e174880a447890dbb6d
Funmi7 <talabifunmilayo@gmail.com>
2019-11-18 15:40:49 +0100

    Created boilerplate

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// require('dotenv').config()
-
-// const server = require('./server')
-// const port = require('./config').port
-
-// server.listen(port, () => {
-//   console.log('listening on ' + port)
-// })
 
 const server = require('./server');
 
