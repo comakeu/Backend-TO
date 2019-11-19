dd5f2c3abc0dd97ddc7178ff344ad23f5eee2f11
Funmi7 <talabifunmilayo@gmail.com>
2019-11-19 08:41:44 +0100

    Added login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,7 +11,7 @@ router.post('/register', (req, res) => {
   Users.add(user)
   .then(addedUSer => {
     res.status(201).json({
-      message: `${addedUSer.user} User registered successfully`
+      message: `${addedUSer.first_name} registered successfully`
     })
   })
   .catch(error => {
@@ -19,4 +19,41 @@ router.post('/register', (req, res) => {
   });
 });
 
+router.post('/login', (req, res) => {
+  let {email, password} = req.body;
+  Users.findBy({ email})
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({
+        message: `Welcome ${user.first_name}`,
+        token: token
+      })
+    } else {
+      res.status(401).json({
+        message: 'Invalid credentials'
+      })
+      .catch(error => {
+        res.status(500).json(error.message)
+      })
+    }
+  })
+})
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    first_name: user.first_name
+    } 
+
+    const options = {
+      expiresIn: '1d'
+    }
+
+    const result = jwt.sign(
+      payload,
+      
+    )
+}
 module.exports = router;
