801ccc34dc0d0fd22907daf3c33df45c40fcbce8
Funmi7 <talabifunmilayo@gmail.com>
2019-11-19 13:01:16 +0100

    Added configuration for heroku postgres

--- knexfile.js
+++ knexfile.js
@@ -20,20 +20,19 @@ module.exports = {
     },
   },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  }
 
 };
